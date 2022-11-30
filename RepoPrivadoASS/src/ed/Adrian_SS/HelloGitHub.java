e105ac3dea980d706ae7d8eb5a5e6309644ff75d
adriss94 <adriss94@gmail.com>
2022-11-30 20:05:14 +0100

    [MOD] HelloGitHub.java – main(): Añadido mensaje de bienvenida

--- RepoPrivadoASS/src/ed/Adrian_SS/HelloGitHub.java
+++ RepoPrivadoASS/src/ed/Adrian_SS/HelloGitHub.java
@@ -3,8 +3,8 @@ package ed.Adrian_SS;
 public class HelloGitHub {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hola GitHub, soy Adrian Salvador");
+			
 	}
 
 }
